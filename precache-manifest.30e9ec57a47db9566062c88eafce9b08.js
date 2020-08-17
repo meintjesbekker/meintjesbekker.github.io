f6598de87315ea28547acc3abb3bbf72a6060381
meintjesbekker <meintjesbekker@yahoo.com>
2020-08-17 20:38:01 +0100

    Add website

---
+++ precache-manifest.30e9ec57a47db9566062c88eafce9b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c274df41b48fcf0ac41071b008957ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7427abf470f076d1a45f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c4c65f01245c14b25288",
+    "url": "/static/js/2.d6df0965.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6df0965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7427abf470f076d1a45f",
+    "url": "/static/js/main.50af553d.chunk.js"
+  },
+  {
+    "revision": "17e0db9c55ef971e23a2",
+    "url": "/static/js/runtime-main.4c51d21c.js"
+  }
+]);
